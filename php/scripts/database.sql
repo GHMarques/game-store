8ed7c66c5cdcd0beb720de31677e50443c0fd08c
GHMarques <gustavo.marques2011@gmail.com>
2018-05-31 09:04:26 -0300

    Alter database

--- php/scripts/database.sql
+++ php/scripts/database.sql
@@ -1,6 +1,6 @@
 /*
 Created: 28/04/2018
-Modified: 28/04/2018
+Modified: 01/05/2018
 Model: PostgreSQL 9.5
 Database: PostgreSQL 9.5
 */
@@ -8,10 +8,10 @@ Database: PostgreSQL 9.5
 
 -- Create tables section -------------------------------------------------
 
--- Table client
+-- Table user
 
-CREATE TABLE "client"(
- "id" SERIAL NOT NULL,
+CREATE TABLE "user"(
+ "id" Bigint NOT NULL,
  "name" Character varying(50) NOT NULL,
  "email" Character varying(50) NOT NULL,
  "password" Character varying(20) NOT NULL,
@@ -19,28 +19,28 @@ CREATE TABLE "client"(
  "street" Character varying(20) NOT NULL,
  "number" Character varying(10) NOT NULL,
  "state" Character varying(20) NOT NULL,
- "complement" Character varying(20),
+ "complement" Character varying(20) NOT NULL,
  "country_id" Bigint NOT NULL
 )
 ;
 
--- Create indexes for table client
+-- Create indexes for table user
 
-CREATE INDEX "client_country_relationship" ON "client" ("country_id")
+CREATE INDEX "user_country_relationship" ON "user" ("country_id")
 ;
 
--- Add keys for table client
+-- Add keys for table user
 
-ALTER TABLE "client" ADD CONSTRAINT "Key1" PRIMARY KEY ("id")
+ALTER TABLE "user" ADD CONSTRAINT "Key1" PRIMARY KEY ("id")
 ;
 
-ALTER TABLE "client" ADD CONSTRAINT "email" UNIQUE ("email")
+ALTER TABLE "user" ADD CONSTRAINT "email" UNIQUE ("email")
 ;
 
 -- Table country
 
 CREATE TABLE "country"(
- "id" SERIAL NOT NULL,
+ "id" Bigint NOT NULL,
  "name" Character varying(20) NOT NULL
 )
 ;
@@ -53,7 +53,7 @@ ALTER TABLE "country" ADD CONSTRAINT "Key2" PRIMARY KEY ("id")
 -- Table game
 
 CREATE TABLE "game"(
- "id" SERIAL NOT NULL,
+ "id" Bigint NOT NULL,
  "name" Character varying(20) NOT NULL,
  "release_date" Date NOT NULL,
  "recommended_age" Bigint NOT NULL,
@@ -79,9 +79,10 @@ ALTER TABLE "game" ADD CONSTRAINT "name" UNIQUE ("name")
 -- Table company
 
 CREATE TABLE "company"(
- "id" SERIAL NOT NULL,
+ "id" Bigint NOT NULL,
  "name" Character varying(50) NOT NULL,
- "country_id" Bigint NOT NULL
+ "country_id" Bigint NOT NULL,
+ "money" Double precision DEFAULT 0 NOT NULL
 )
 ;
 
@@ -95,10 +96,11 @@ CREATE INDEX "company_country_relationship" ON "company" ("country_id")
 ALTER TABLE "company" ADD CONSTRAINT "Key4" PRIMARY KEY ("id")
 ;
 
--- Table client_game
+-- Table user_game
 
-CREATE TABLE "client_game"(
- "client_id" SERIAL NOT NULL,
+CREATE TABLE "user_game"(
+ "id" Bigint NOT NULL,
+ "user_id" Bigint NOT NULL,
  "game_id" Bigint NOT NULL,
  "date" Date NOT NULL,
  "total_price" Double precision NOT NULL,
@@ -106,13 +108,13 @@ CREATE TABLE "client_game"(
 )
 ;
 
--- Add keys for table client_game
+-- Add keys for table user_game
 
-ALTER TABLE "client_game" ADD CONSTRAINT "Key5" PRIMARY KEY ("client_id","game_id")
+ALTER TABLE "user_game" ADD CONSTRAINT "Key5" PRIMARY KEY ("user_id","game_id","id")
 ;
 -- Create foreign keys (relationships) section ------------------------------------------------- 
 
-ALTER TABLE "client" ADD CONSTRAINT "client_country" FOREIGN KEY ("country_id") REFERENCES "country" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+ALTER TABLE "user" ADD CONSTRAINT "user_country" FOREIGN KEY ("country_id") REFERENCES "country" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 ;
 
 ALTER TABLE "company" ADD CONSTRAINT "company_country" FOREIGN KEY ("country_id") REFERENCES "country" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
@@ -121,8 +123,11 @@ ALTER TABLE "company" ADD CONSTRAINT "company_country" FOREIGN KEY ("country_id"
 ALTER TABLE "game" ADD CONSTRAINT "game_company" FOREIGN KEY ("company_id") REFERENCES "company" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 ;
 
-ALTER TABLE "client_game" ADD CONSTRAINT "client_game" FOREIGN KEY ("client_id") REFERENCES "client" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+ALTER TABLE "user_game" ADD CONSTRAINT "user_game" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 ;
 
-ALTER TABLE "client_game" ADD CONSTRAINT "game_client" FOREIGN KEY ("game_id") REFERENCES "game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+ALTER TABLE "user_game" ADD CONSTRAINT "game_user" FOREIGN KEY ("game_id") REFERENCES "game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 ;
+
+
+
